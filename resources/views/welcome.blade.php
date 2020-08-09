c83c17259c715809627b0703181ada0d5e7497b4
Francisco Rosas <rosaf2@student.op.ac.nz>
2020-08-09 17:56:26 +1200

    Edit of main title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ABC
+                    tfghkjblk
                 </div>
 
                 <div class="links">
